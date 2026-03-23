9182a32b49f01fc09498c064f188e82ed6f41b10
Leah B <leahb2613@gmail.com>
2026-03-23 16:07:08 -0500

    started adding callout

--- mobile/src/screens/IssueListScreen.tsx
+++ mobile/src/screens/IssueListScreen.tsx
@@ -1,27 +1,18 @@
 // mobile/src/screens/IssueListScreen.tsx
-import { View, RefreshControl, Text, StyleSheet } from 'react-native';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { useState, useContext } from 'react';
-import { FlatList } from 'react-native';
+import { View, RefreshControl } from 'react-native';
+import { useState } from 'react';
 import React from 'react';
 import IssueCard from '../components/IssueCard';
 import { MessageView } from '../components/MessageView';
-import { userLocation } from '../types/userLocation';
-import { LocationContext } from '../types/LocationContext';
-import ENV from '../config/env';
 import { useNavigation } from '@react-navigation/native';
 import { StackParams } from '../types/StackParams';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { globalStyles, spacing, size } from '../styles';
-import LoadingScreen from './LoadingScreen';
-import BottomSheet, { BottomSheetFlatList, BottomSheetView } from '@gorhom/bottom-sheet';
+import { globalStyles, spacing } from '../styles';
+import { BottomSheetFlatList } from '@gorhom/bottom-sheet';
 
 export default function IssueListScreen({ issues, refetch }: any) {
   const [refreshing, setRefreshing] = useState(false)
   const navigation = useNavigation<StackNavigationProp<StackParams>>();
-
-  //get contexts from above layer(s)
-
 
   //check if any data was returned
   if (issues.length == 0) {
